adaabfadb5098e0a1c656b6d6ceeaefa3818cbaf
Erik Borge <erikaasborge@gmail.com>
2022-09-04 21:51:52 +0200

    added sanity credentials to env

--- public/src/sanity-client.js
+++ public/src/sanity-client.js
@@ -2,8 +2,8 @@ import sanityClient from "@sanity/client";
 
 export default sanityClient({
   // Find your project ID and dataset in `sanity.json` in your studio project
-  projectId: "wbni95pr",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: true,
   apiVersion: "2022-03-25",
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
